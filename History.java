a6bc0691436b19b431a847a17bdce36be7aae24e
PorCheu <porcheu.leng@student.cadt.edu.kh>
2025-01-24 12:15:02 +0700

    my history update

--- History.java
+++ History.java
@@ -1,1 +1,17 @@
-hi
+// History.java
+public class History {
+    String historyId;
+    String transactionType; // Example: "Loan Repayment", "Loan Disbursement"
+    String date;
+    double amount;
+    String loanId; // Reference to the associated loan
+
+    // Constructor
+    public History(String historyId, String transactionType, String date, double amount, String loanId) {
+        this.historyId = historyId;
+        this.transactionType = transactionType;
+        this.date = date;
+        this.amount = amount;
+        this.loanId = loanId;
+    }
+}
