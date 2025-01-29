5dfe1f39a51588d600f33aa76605b6b10b9856b6
Tsn168 <163022251+Tsn168@users.noreply.github.com>
2025-01-29 21:31:28 +0700

    hi

--- Notification.java
+++ Notification.java
@@ -1,5 +1,13 @@
-public class Notification{
+
+public class Notification {
+
     int user_ID;
     String message;
     String time;
+
+    Notification(int user_ID, String message, String time {
+        this.user_ID = user_ID;
+        this.message = message;
+        this.time = time;
+    }
 }
