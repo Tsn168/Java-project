454c7e41809bcab41f443604c4f7b97632b4f2c0
Tsn168 <163022251+Tsn168@users.noreply.github.com>
2025-02-07 14:29:46 +0700

    commiting update

--- Notification.java
+++ Notification.java
@@ -1,13 +1,55 @@
-
 public class Notification {
-
-    int user_ID;
-    String message;
-    String time;
-
-    Notification(int user_ID, String message, String time) {
+    static int allMessage = 0; 
+    private int user_ID;
+    private String message;
+    private String time;
+    public Notification(int user_ID, String message, String time) {
         this.user_ID = user_ID;
         this.message = message;
         this.time = time;
+        allMessage++;
+    }
+    public void displayNotification() {
+        System.out.println("User ID: " + user_ID);
+        System.out.println("Message: " + message);
+        System.out.println("Time: " + time);
+        System.out.println("-----------------------------");
+    }
+    public int getUserID() {
+        return user_ID;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public static int getTotalMessages() {
+        return allMessage;
+    }
+    public boolean containsKeyword(String keyword) {
+        return message.toLowerCase().contains(keyword.toLowerCase());
+    }
+    public String getFormattedNotification() {
+        return " [User " + user_ID + "] " + message + " (at " + time + ")";
+    }
+    
+    public String toString() {
+        return "Notification{" +
+               "user_ID=" + user_ID +
+               ", message='" + message + '\'' +
+               ", time='" + time + '\'' +
+               '}';
     }
 }
